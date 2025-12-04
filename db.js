943688457301448b223a52c75d512e3105ac636b
David Gamboa <davidgamboa317@gmail.com>
2025-12-03 19:13:22 -0600

    final4

--- db.js
+++ db.js
@@ -1,27 +1,15 @@
-const mysql = require("mysql2");
+import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST_CLEVER,
   user: process.env.DB_USER_CLEVER,
-  password: process.env.DB_PASSWORD_CLEVER,
+  password: process.env.DB_PASS_CLEVER,
   database: process.env.DB_NAME_CLEVER,
-  port: process.env.DB_PORT_CLEVER || 3306,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
-  connectionLimit: 5,  
+  connectionLimit: 3,   // Clever Cloud solo deja máximo 5
   queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false }
 });
 
-// Probar conexión
-pool.getConnection((err, conn) => {
-  if (err) {
-    console.error("❌ Error al conectar a MySQL:", err);
-    return;
-  }
-  console.log("✅ Conexión establecida con MySQL (pool)");
-  conn.release();
-});
-
-module.exports = pool;
+export default pool;
