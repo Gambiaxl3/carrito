42303602ca3453414b4cc7ec51b8240c4d4e96ef
David Gamboa <davidgamboa317@gmail.com>
2025-12-03 19:05:44 -0600

    final1

--- db.js
+++ db.js
@@ -1,27 +1,14 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST_CLEVER,
-  user: process.env.DB_USER_CLEVER,
-  password: process.env.DB_PASSWORD_CLEVER,
-  database: process.env.DB_NAME_CLEVER,
-  port: process.env.DB_PORT_CLEVER || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectionLimit: 5, // 💥 IMPORTANTE: NO usar más conexiones que las permitidas
   waitForConnections: true,
-  connectionLimit: 5,  
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-// Probar conexión
-pool.getConnection((err, conn) => {
-  if (err) {
-    console.error("❌ Error al conectar a MySQL:", err);
-    return;
-  }
-  console.log("✅ Conexión establecida con MySQL (pool)");
-  conn.release();
+  queueLimit: 0
 });
 
 module.exports = pool;
